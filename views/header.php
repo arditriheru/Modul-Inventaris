ac8c27e642cc2c68b0ed0d1883382c756fd126ad
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-28 15:41:59 +0700

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,55 @@
+<?php
+    error_reporting(0);
+    session_start();
+    $update = $_SESSION['tanggal'];
+    $jam    = $_SESSION['jam'];
+        function format_update($update)
+            {
+                $bulan = array (1 =>   'Januari',
+                    'Februari',
+                    'Maret',
+                    'April',
+                    'Mei',
+                    'Juni',
+                    'Juli',
+                    'Agustus',
+                    'September',
+                    'Oktober',
+                    'November',
+                    'Desember'
+                    );
+                $split = explode('-', $update);
+                return $split[2] . ' ' . $bulan[ (int)$split[1] ] . ' ' . $split[0];
+        }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SIMETRIS | Inventaris</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Add custom CSS here -->
+    <link href="../css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/sweetalert.css">
+    <style>
+    .bluetext {
+    color: #008cba;
+    }
+    .redtext {
+    color: #e71414;
+    }
+    .navbar-rachmi{
+        background-color:#e67e22;
+        border-color:#d35400
+    }
+    .navbar-brand{
+        color:#ffffff;
+    }
+
+</style>
+  </head>
